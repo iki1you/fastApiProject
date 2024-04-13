125fa9f0a651823ce540b76ca7abc436034d5e4c
Krabs <vovastr0v@yandex.ru>
2024-04-13 22:46:34 +0500

    fixes

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from starlette.responses import JSONResponse
 
 def get_from_json(filename: str):
     try:
-        with open(filename, 'r') as news_file:
+        with open(f'json_data/{filename}', 'r') as news_file:
             return json5.load(news_file)
     except BaseException:
         return JSONResponse(status_code=404, content=f"ошибка декодирования json: '{filename}'")
